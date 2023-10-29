962fe25d7dba2dc3896b1d1c4c3bd834c7040491
martinsoengas <martin.soengas@loesen.co>
2023-10-29 19:34:53 -0300

    add DB connection

---
+++ src/config/db.ts
@@ -0,0 +1,14 @@
+// Package Imports
+import mongoose, { ConnectOptions } from 'mongoose';
+
+if (!process.env.MDB_URL) {
+  throw new Error('MDB_URL environment variable is not defined!');
+}
+
+// Mongoose Connection
+mongoose
+  .connect(process.env.MDB_URL, { dbName: 'logipartes' } as ConnectOptions)
+  .then((res) => console.log('💻 Mongoose Connected'))
+  .catch((err) => console.log('🛑 Mongoose Failed: ' + err));
+
+export default mongoose;
